16c9b1b2c902624f74cdabad065d29dbca69c9a2
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-11 17:31:10 +0900

    re fix books/dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/dashboard' => to: 'books#dashboard'
+  get '/dashboard', to: 'books#dashboard'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
