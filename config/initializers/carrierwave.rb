c85df586061ae8c9087babfc3c13e55ffff198b2
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-06 23:25:56 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,6 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
 
   config.storage = :fog
-  config.fog_provider = "fog/aws"
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY"],
