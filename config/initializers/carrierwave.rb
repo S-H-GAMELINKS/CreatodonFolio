d8d7a670045aa44cdf7a38089014441d9151c02f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-07 10:52:22 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
 CarrierWave.configure do |config|
-
-  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY"],
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],
     region: ENV["AWS_REGION_NAME"]
   }
+  config.storage = :fog
 
   case Rails.env
     when 'production'
