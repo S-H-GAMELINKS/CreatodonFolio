418d4992a2519990c8795f74ac5db3ee23482480
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-02 20:38:23 +0900

    fix macy.js

---
+++ app/javascript/packs/macy.js
@@ -0,0 +1,16 @@
+var macy = Macy({
+    container: '#macy-container',
+    trueOrder: false,
+    waitForImages: false,
+    columns: 6,
+    margin: {
+        x: 10,
+        y: 5,
+    },
+    breakAt: {
+        1200: 5,
+        940: 3,
+        520: 2,
+        400: 1
+    }
+});
