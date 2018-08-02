54e58f0b69e89ac8eef08ff73780d0e5c9157cf6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-02 22:35:37 +0900

    fix fog-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,26 +1,12 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY"],
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
-    region: ENV["AWS_REGION_NAME"]
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region:                ENV['AWS_REGION_NAME'],
   }
-
-  case Rails.env
-    when 'production'
-      config.fog_directory = ENV["AWS_STORAGE_NAME"]
-      config.asset_host = ENV["AWS_STORAGE_PATH"]
-
-    when 'development'
-      config.fog_directory = ENV["AWS_STORAGE_NAME"]
-      config.asset_host = ENV["AWS_STORAGE_PATH"]
-
-    when 'test'
-      config.fog_directory = ENV["AWS_STORAGE_NAME"]
-      config.asset_host = ENV["AWS_STORAGE_PATH"]
-  end
+  config.fog_directory  = ENV['AWS_STORAGE_NAME'] 
+  config.fog_public     = false
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
