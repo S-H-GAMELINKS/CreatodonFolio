c27240d2351ea20895c6e2286d3353db5ec089c6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-02 17:39:23 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,3 @@
 //= require_tree .
 //= require serviceworker-companion
 //= require macy/dist/macy
-
-var macy = Macy({
-    container: '#macy-container',
-    trueOrder: false,
-    waitForImages: false,
-    columns: 6,
-    margin: {
-        x: 20,
-        y: 10,
-      },
-    breakAt: {
-        1200: 5,
-        940: 3,
-        520: 2,
-        400: 1
-    }
-});
