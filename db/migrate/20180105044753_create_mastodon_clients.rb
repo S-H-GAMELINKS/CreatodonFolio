0d348d0388a84563d1ea98266bc0b86805852343
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-02 23:27:42 +0900

    fix migration

--- db/migrate/20180105044753_create_mastodon_clients.rb
+++ db/migrate/20180105044753_create_mastodon_clients.rb
@@ -1,5 +1,6 @@
 class CreateMastodonClients < ActiveRecord::Migration[5.2]
   def change
+    drop_table :mastodon_clients
     create_table :mastodon_clients do |t|
       t.string :domain
       t.string :client_id
